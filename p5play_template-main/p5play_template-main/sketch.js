7a7a6394936a94aa3133393ad1c787be6b7cc5d2
mini_shimata B) <96700462+minishimata@users.noreply.github.com>
2021-12-26 17:18:05 -0300

    Add files via upload

---
+++ p5play_template-main/p5play_template-main/sketch.js
@@ -0,0 +1,19 @@
+var bolinha;
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+  bolinha = createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background("white");
+drawSprites();
+}
+
+
+
+
